8aff3e022d884f5e736d08cfbfda14def1f63fd9
k-vernooy <kai.a.vernooy@gmail.com>
2020-09-27 00:03:29 -0400

    Add basic files for merge

---
+++ web/static/js/load_data.js
@@ -0,0 +1,14 @@
+function loadData() {
+    // read from file, fill
+    // global json
+    openPage();
+}
+
+var Landing = document.getElementById("landing");
+function openPage() {
+    var h1 = Landing.querySelectorAll("h1, h2")
+    console.log(h1);
+}
+
+
+loadData();
